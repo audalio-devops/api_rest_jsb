6508f28d7d5dfb1e0cfc252ab422c8f00460075d
cpqd_audalio <audalio@cpqd.com.br>
2024-04-10 14:15:05 -0300

    Adicionando o Plugin Lombok

--- projeto/src/main/java/br/com/ajrdevops/projeto/model/Usuario.java
+++ projeto/src/main/java/br/com/ajrdevops/projeto/model/Usuario.java
@@ -1,7 +1,12 @@
 package br.com.ajrdevops.projeto.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.scheduling.support.SimpleTriggerContext;
+
+@Data
 
 @Entity
 @Table(name = "usuario")
@@ -20,44 +25,5 @@ public class Usuario {
     @Column(name = "telefone", length = 15)
     private String telefone;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
 
 }
