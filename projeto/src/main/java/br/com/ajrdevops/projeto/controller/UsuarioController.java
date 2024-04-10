227fb2ebcfc89e1f2c13b6072a19eac86424c093
cpqd_audalio <audalio@cpqd.com.br>
2024-04-10 14:53:22 -0300

    Validando os campos com o Spring Boot Validation

--- projeto/src/main/java/br/com/ajrdevops/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/ajrdevops/projeto/controller/UsuarioController.java
@@ -3,12 +3,18 @@ package br.com.ajrdevops.projeto.controller;
 import br.com.ajrdevops.projeto.repository.IUsuario;
 import br.com.ajrdevops.projeto.model.Usuario;
 import br.com.ajrdevops.projeto.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -28,11 +34,11 @@ public class UsuarioController {
     }
 
     @PostMapping
-    public ResponseEntity<Usuario> criarUsuario(@RequestBody Usuario usuario) {
+    public ResponseEntity<Usuario> criarUsuario(@Valid @RequestBody Usuario usuario) {
         return ResponseEntity.status(201).body(usuarioService.criarUsuario(usuario));
     }
     @PutMapping
-    public ResponseEntity<Usuario>  editarUsuario(@RequestBody Usuario usuario) {
+    public ResponseEntity<Usuario>  editarUsuario(@Valid @RequestBody Usuario usuario) {
         return ResponseEntity.status(200).body(usuarioService.editarUsuario(usuario));
     }
 
@@ -43,11 +49,25 @@ public class UsuarioController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Usuario> validarSenha(@RequestBody Usuario usuario) {
+    public ResponseEntity<Usuario> validarSenha(@Valid @RequestBody Usuario usuario) {
         Boolean valid = usuarioService.validarSenha(usuario);
         if (!valid) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         return ResponseEntity.status(200).build();
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handlleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) ->{
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
--- projeto/src/main/java/br/com/ajrdevops/projeto/model/Usuario.java
+++ projeto/src/main/java/br/com/ajrdevops/projeto/model/Usuario.java
@@ -1,6 +1,7 @@
 package br.com.ajrdevops.projeto.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -16,12 +17,22 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
+
+    @NotBlank(message = "O nome é obrigatório!")
+    @Size(min = 3, message = "O nome deve ter no mínimo 3 caracteres!")
     @Column(name = "nome", length = 200, nullable = false)
     private String nome;
+
+    @Email(message = "Insira um email válido!")
+    @NotBlank(message = "O email é obrigatório!")
     @Column(name = "email", length = 50, nullable = false)
     private String email;
+
+    @NotBlank(message = "A senha é obrigatória!")
     @Column(name = "senha", columnDefinition = "TEXT", nullable = false)
     private String senha;
+
+    @NotBlank(message = "O telefone é obrigatório!")
     @Column(name = "telefone", length = 15)
     private String telefone;
 
