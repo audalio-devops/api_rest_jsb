416d0aee64a72eab4c58d902161655c91eff3fc6
audalio-devops <audalio.devops@gmail.com>
2024-04-10 00:40:55 -0300

    Inclusão da dependência Security do Spring Boot e validação da senha

--- projeto/src/main/java/br/com/ajrdevops/projeto/controller/UsuarioController.java
+++ projeto/src/main/java/br/com/ajrdevops/projeto/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import br.com.ajrdevops.projeto.repository.IUsuario;
 import br.com.ajrdevops.projeto.model.Usuario;
 import br.com.ajrdevops.projeto.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -39,5 +40,14 @@ public class UsuarioController {
     public ResponseEntity<?> excluirUsuario(@PathVariable Integer id) {
         usuarioService.excluirUsuario(id);
         return ResponseEntity.status(204).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Usuario> validarSenha(@RequestBody Usuario usuario) {
+        Boolean valid = usuarioService.validarSenha(usuario);
+        if (!valid) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.status(200).build();
     }
 }
